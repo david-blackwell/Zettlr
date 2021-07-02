78ae493f047320b0274a470d6ccecbbb34a27b4e
david-blackwell <david.blackwell@adelaide.edu.au>
2021-07-02 14:13:00 +0930

    Edit foldcode-helper.js to employ code to fold header sections to fold entire code blocks, similar to RStudio

--- source/renderer/modules/markdown-editor/plugins/foldcode-helper.js
+++ source/renderer/modules/markdown-editor/plugins/foldcode-helper.js
@@ -20,29 +20,72 @@
   'use strict'
 
   CodeMirror.registerHelper('fold', 'markdown', function (cm, start) {
-    var maxDepth = 100
+    const maxDepth = 100
 
     function isHeader (lineNo) {
-      var tokentype = cm.getTokenTypeAt(CodeMirror.Pos(lineNo, 0))
+      const tokentype = cm.getTokenTypeAt(CodeMirror.Pos(lineNo, 0))
       return tokentype && /\bheader\b/.test(tokentype)
     }
 
     function headerLevel (lineNo, line, nextLine) {
-      var match = line && line.match(/^#+/)
+      let match = line && line.match(/^#+/)
       if (match && isHeader(lineNo)) return match[0].length
       match = nextLine && nextLine.match(/^[=-]+\s*$/)
       if (match && isHeader(lineNo + 1)) return nextLine[0] === '=' ? 1 : 2
       return maxDepth
     }
 
-    var firstLine = cm.getLine(start.line)
-    var nextLine = cm.getLine(start.line + 1)
-    var level = headerLevel(start.line, firstLine, nextLine)
+    if(cm.getLine(start.line).match(/^```+/)){
+      const maxDepth = 100
+
+    function isFence (lineNo) {
+      // const tokentype = cm.getTokenTypeAt(CodeMirror.Pos(lineNo, 0))
+      // return tokentype && /\bheader\b/.test(tokentype)
+      const tokentype = cm.getTokenTypeAt(CodeMirror.Pos(lineNo, 0))
+      //console.log(cm.getTokenTypeAt(CodeMirror.Pos(0, 0)))
+      //console.log(/\bcode\b/.test(cm.getTokenTypeAt(CodeMirror.Pos(0, 0))))
+      return tokentype && /\bcode\b/.test(tokentype)
+    }
+
+    function fenceLevel (lineNo, line, nextLine) {
+      // let match = line && line.match(/^#+/)
+      let match = line && line.match(/^```+/)
+      if (match && isFence(lineNo)) return match[0].length
+      match = nextLine && nextLine.match(/^[=-]+\s*$/)
+      if (match && isFence(lineNo + 1)) return nextLine[0] === '=' ? 1 : 2
+      return maxDepth
+    }
+
+    const firstLine = cm.getLine(start.line)
+    let nextLine = cm.getLine(start.line + 1)
+    const level = fenceLevel(start.line, firstLine, nextLine)
     if (level === maxDepth) return undefined
 
-    var lastLineNo = cm.lastLine()
-    var end = start.line
-    var nextNextLine = cm.getLine(end + 2)
+    const lastLineNo = cm.lastLine()
+    let end = start.line
+    let nextNextLine = cm.getLine(end + 2)
+    while (end < lastLineNo) {
+      if (fenceLevel(end + 1, nextLine, nextNextLine) <= level) break
+      ++end
+      nextLine = nextNextLine
+      nextNextLine = cm.getLine(end + 2)
+    }
+
+    return {
+      from: CodeMirror.Pos(start.line, firstLine.length),
+      to: CodeMirror.Pos(end, cm.getLine(end).length)
+    }
+    }
+    else{
+
+    const firstLine = cm.getLine(start.line)
+    let nextLine = cm.getLine(start.line + 1)
+    const level = headerLevel(start.line, firstLine, nextLine)
+    if (level === maxDepth) return undefined
+
+    const lastLineNo = cm.lastLine()
+    let end = start.line
+    let nextNextLine = cm.getLine(end + 2)
     while (end < lastLineNo) {
       if (headerLevel(end + 1, nextLine, nextNextLine) <= level) break
       ++end
@@ -54,5 +97,6 @@
       from: CodeMirror.Pos(start.line, firstLine.length),
       to: CodeMirror.Pos(end, cm.getLine(end).length)
     }
+  }
   })
 })
